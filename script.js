70ef6da5d103c0b6ac3d25fa943965807e44a617
MoonLightCodes <poornachander295@gmail.com>
2025-01-18 19:25:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	str=str.split('').sort().join('');
+	let i =0;
+	let j =1;
+	while(i<str.length){
+		j=i+1;
+		let c=0
+		while(str[i]===str[j]){
+			j++;
+			c++;
+		}
+		if(c===0)return str[i];
+		i=j;
+	}
+	return null
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
